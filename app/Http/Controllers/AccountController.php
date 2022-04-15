dd9c509a35b577d59eccb1131f47e0969d497a98
jose dev <josededanes99@gmail.com>
2022-04-15 13:52:50 -0600

    add new server

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -40,7 +40,8 @@ class AccountController extends Controller
             get();
         $resp_data = $getUserAll[0];
         //Create user a server ssh
-        $this->command_ssh(get_days(),$resp_data->ip,'hive-vpn.tk-'.$data['user'],$data['passwd']);
+        //Create user a server ssh
+        $this->command_ssh(get_days(),$resp_data->ip,'hive-vpn.tk-'.session('user'), session('passwd'),$resp_data->vps_user,$resp_data->vps_passwd);
         
         return view('view_account',compact('resp_data'));
         
@@ -52,9 +53,9 @@ class AccountController extends Controller
             get();
         return view('panel.ssh.index',compact('getUsersAll'));
     }
-    public function command_ssh($date,$ip,$user,$passwd){
+    public function command_ssh($date,$ip,$user,$passwd,$vps_user,$vps_passwd){
         $comand = 'useradd -e '.$date.' -p "$(mkpasswd --method=sha-512 '.$passwd.')" '.$user;
         
-        $exec = ssh2_exec(connect($ip,'vps-2021-hive',22), $comand);
+        $exec = ssh2_exec(connect($ip,$vps_user,$vps_passwd,22), $comand);
     }
 }
--- database/seeds/ServerSeeder.php
+++ database/seeds/ServerSeeder.php
@@ -14,15 +14,43 @@ class ServerSeeder extends Seeder
     {
         server::create([
             'name' => 'Estados Unidos',
-            'payload' => 'HTTP/1.1 wss//',
+            'payload' => 'GET shi://sni.cloudflaressl.com HTTP/1.1[crlf]Host: vip1.hivevpn.tk[crlf]Upgrade: websocket[crlf][crlf]',
             'ip' => '157.245.11.150',
             'country' => 'USA',
             'province' => 'new york',
+            'days' => 31,
+            'price' => 4,
+            'type' => 'premium',
+            'limit' => 50,
+            'domain' => 'vip1.hivevpn.tk',
+            'vps_user' => 'root',
+            'vps_passwd' => 'vps-2021-hive'
+        ]);
+        server::create([
+            'name' => 'Estados Unidos',
+            'payload' => 'GET shi://sni.cloudflaressl.com HTTP/1.1[crlf]Host: usa1.vip-cloud.tk[crlf]Upgrade: websocket[crlf][crlf]',
+            'ip' => '138.197.65.194',
+            'country' => 'USA',
+            'province' => 'New York',
             'days' => 3,
             'price' => 3,
             'type' => 'free',
             'limit' => 100,
-            'domain' => 'vip-cloud.tk',
+            'domain' => 'usa1.vip-cloud.tk',
+            'vps_user' => 'root',
+            'vps_passwd' => 'vps-2021-hive'
+        ]);
+        server::create([
+            'name' => 'Estados Unidos',
+            'payload' => 'GET shi://sni.cloudflaressl.com HTTP/1.1[crlf]Host: sf.vpn-internet.tk[crlf]Upgrade: websocket[crlf][crlf]',
+            'ip' => '143.244.188.234',
+            'country' => 'USA',
+            'province' => 'San Francisco',
+            'days' => 3,
+            'price' => 0,
+            'type' => 'free',
+            'limit' => 50,
+            'domain' => 'sf.vpn-internet.tk',
             'vps_user' => 'root',
             'vps_passwd' => 'vps-2021-hive'
         ]);
