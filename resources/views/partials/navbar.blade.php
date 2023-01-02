7e0e17e106c32490c00cb4767159cc1b69e35167
josedeodanes99 <josedeodanes99@gmail.com>
2023-01-02 15:36:21 -0600

    Branch venta version

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="{{ asset('images/icons/logo.svg') }}" height="25" alt="">
+            <span style="font-weight: bold; color;">HIRVIP-VPN</span>
         </a>
         <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
